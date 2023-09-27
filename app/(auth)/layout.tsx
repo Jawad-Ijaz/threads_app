79fc6ca1f8743cee7c771832db0ddb694669aead
Jawad Ijaz <jawad.ijaz@codeninjaconsulting.com>
2023-09-27 10:51:59 +0500

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,40 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google';
+import '../globals.css';
+
+
+export const metadata = {
+    title: 'Threads',
+    description: 'A Next.js 13 Meta Threads Application'
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+    children
+}: {
+
+    children: React.ReactNode
+}) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={inter.className}>
+
+                    <main>
+
+                        <section className='main-container'>
+                            <div className="w-full flex justify-center items-center min-h-screen max-w-4xl">
+                                {children}
+                            </div>
+                        </section>
+
+                    </main>
+
+                </body>
+            </html>
+        </ClerkProvider>
+
+
+    )
+}
